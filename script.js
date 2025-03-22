cf864d5b17e84195e23b67bde19246f7e3b41bd3
dilayaslan <dilaymavi@gmail.com>
2025-03-22 19:46:16 +0300

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const homeworks = [
     "Homework 1: C Programı - Basit Hesap Makinesi",
     "Homework 2: C Programı - Fibonacci Dizisi",
     "Homework 3: Web Programlama - HTML & CSS ile Basit Web Sayfası"
+    "Homework 4: Basit Web Sayfası"
 ];
 
 // Adınızı HTML'ye ekliyoruz
