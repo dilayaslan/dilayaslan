660a674bb34d5a7f816fd4690e36ae4c8c9dbe46
dilayaslan <dilaymavi@gmail.com>
2025-03-22 19:41:03 +0300

    Update script.js

--- script.js
+++ script.js
@@ -18,4 +18,18 @@ document.getElementById('homework1').addEventListener('click', function() {
     link.href = 'homework1.c';  // Burada dosyanızın doğru yolunu belirtiyoruz
     link.download = 'homework1.c';  // İndirilen dosyanın adı
     link.click();
-});
+document.getElementById('homework2').addEventListener('click', function() {
+    const link = document.createElement('a');
+    link.href = 'homework2.c';  // Burada dosyanızın doğru yolunu belirtiyoruz
+    link.download = 'homework2.c';  // İndirilen dosyanın adı
+    link.click();
+document.getElementById('homework3').addEventListener('click', function() {
+    const link = document.createElement('a');
+    link.href = 'homework3.c';  // Burada dosyanızın doğru yolunu belirtiyoruz
+    link.download = 'homework3.c';  // İndirilen dosyanın adı
+    link.click();
+document.getElementById('homework4').addEventListener('click', function() {
+    const link = document.createElement('a');
+    link.href = 'homework4.c';  // Burada dosyanızın doğru yolunu belirtiyoruz
+    link.download = 'homework4.c';  // İndirilen dosyanın adı
+    link.click();
